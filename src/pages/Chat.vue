8e7e05f502b380720779d17f6745ee47cae2393a
Martin <martingf@gmail.com>
2025-05-12 19:49:20 -0300

    Último cambio en lo de Fran 12/5 a las 7:49

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -2,10 +2,37 @@
 import MainH1 from "../components/MainH1.vue";
 export default {
   name: "Chat",
-  components: {MainH1},
+  components: { MainH1 },
 };
 </script>
 
 <template>
   <MainH1>Chat global</MainH1>
+  <div class="flex gap-4">
+    <section class="w-9/12">
+      <h2 class="sr-only">Lista de mensajes</h2>
+      <div class="min-h-[400px] p-4 border rounded">
+        <!-- mensajes -->
+      </div>
+    </section>
+    <section class="w-3/12">
+      <h2 class="mb-4 text-lg font-bold">Enviar un mensaje</h2>
+      <form action="#">
+        <div class="mb-4">
+          <label for="user" class="block mb-2">Nombre de usuario</label>
+          <input type="text" id="user" class="w-full border rounded py-2 px-4">
+        </div>
+        <div class="mb-4">
+          <label for="text" class="block mb-2">Mensaje</label>
+          <textarea type="text" id="text" class="w-full border rounded py-2 px-4"></textarea>
+        </div>
+        <button 
+          type="sumbit" 
+          class="py-2 px-4 rounded bg-emerald-700 text-white"
+        >
+          Enviar
+        </button>
+      </form>
+    </section>
+  </div>
 </template>
