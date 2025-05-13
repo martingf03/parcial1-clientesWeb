50b02f6390c0b6179ceadd4f3c4aaea060826412
Martin <martingf@gmail.com>
2025-05-13 15:09:57 -0300

    Cambios en transiciones del navbar, cambios en chatSection. 13/5 3.09

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -7,16 +7,16 @@ export default {
 <template>
   <nav class="bg-emerald-400 border-b-4 border-b-emerald-500">
     <ul class="flex items-center gap-8 p-4 font-[Lexend] text-lg">
-      <li class="hover:text-white transition-colors duration-200 ease-in-out">
+      <li class="hover:text-white transition duration-200 ease-in-out">
         <router-link to="/">Home</router-link>
       </li>
-      <li class="hover:text-white transition-colors duration-200 ease-in-out">
+      <li class="hover:text-white transition duration-200 ease-in-out">
         <router-link to="/chat">Chat</router-link>
       </li>
-      <li class="hover:text-white transition-colors duration-200 ease-in-out">
+      <li class="hover:text-white transition duration-200 ease-in-out">
         <router-link to="/registro">Registro</router-link>
       </li>
-      <li class="hover:text-white transition-colors duration-200 ease-in-out">
+      <li class="hover:text-white transition duration-200 ease-in-out">
         <router-link to="/iniciar-sesion">Iniciar Sesión</router-link>
       </li>
     </ul>
--- src/components/chat/ChatSection.vue
+++ src/components/chat/ChatSection.vue
@@ -2,14 +2,15 @@
 
 export default {
     name: "Chat",
-    
+
     data() {
         return {
-            messages: [],                
+            messages: [],
 
             newMessage: {
                 email: "",
                 content: "",
+                date: "",
             }
         }
     },
@@ -17,8 +18,13 @@ export default {
     methods: {
         SendMessage() {
             this.messages.push({
+                id: this.messages.length + 1,
                 email: this.newMessage.email,
                 content: this.newMessage.content,
+                date: new Date().toLocaleString('es-AR', {
+                    dateStyle: 'short',
+                    timeStyle: 'short'
+                }),
             });
             this.newMessage.content = "";
         }
@@ -27,47 +33,37 @@ export default {
 </script>
 
 <template>
-  <div class="flex gap-4">
+    <div class="flex gap-4">
 
-    <section class="w-9/12">
-      <h2 class="sr-only">Lista de mensajes</h2>
-      <div class="min-h-[400px] p-4 border rounded">
-        <div v-for="m in messages" class="mb-4">
-          <div class="text-sm"><span class="font-bold">{{ m.email }} dijo:</span></div>
-          <div>{{ m.content }}</div>
-        </div>
-      </div>
-    </section>
-    
-    <section class="w-3/12">
-      <h2 class="mb-4 text-lg font-bold">Enviar un mensaje</h2>
-      <form 
-      action="#"
-      @submit.prevent="SendMessage"
-      >
-        <div class="mb-4">
-          <label for="user" class="block mb-2">Nombre de usuario</label>
-          <input 
-          type="text" 
-          id="user" 
-          class="w-full border rounded py-2 px-4"
-          v-model="newMessage.email"
-          >
-        </div>
-        <div class="mb-4">
-          <label for="text" class="block mb-2">Mensaje</label>
-          <textarea 
-          type="text" 
-          id="text" 
-          class="w-full border rounded py-2 px-4"
-          v-model="newMessage.content"
-          >
+        <section class="w-9/12">
+            <h2 class="sr-only">Lista de mensajes</h2>
+            <div class="min-h-[400px] p-4 border rounded">
+                <div v-for="m in messages" :key="m.id" class="mb-4">
+                    <div class="text-sm"><span class="font-bold text-emerald-600">{{ m.email }} dijo:</span></div>
+                    <div>{{ m.content }}</div>
+                    <div class="text-xs text-gray-400">{{ m.date }}</div>
+                </div>
+            </div>
+        </section>
+
+        <section class="w-3/12">
+            <h2 class="mb-4 text-lg font-bold">Enviar un mensaje</h2>
+            <form action="#" @submit.prevent="SendMessage">
+                <div class="mb-4">
+                    <label for="user" class="block mb-2">Nombre de usuario</label>
+                    <input type="text" id="user" class="w-full border rounded py-2 px-4" v-model="newMessage.email">
+                </div>
+                <div class="mb-4">
+                    <label for="text" class="block mb-2">Mensaje</label>
+                    <textarea type="text" id="text" class="w-full border rounded py-2 px-4"
+                        v-model="newMessage.content">
         </textarea>
-        </div>
-        <button type="sumbit" class="py-2 px-4 rounded bg-emerald-700 text-white">
-          Enviar
-        </button>
-      </form>
-    </section>
-  </div>
+                </div>
+                <button type="sumbit"
+                    class="transition py-2 px-4 rounded bg-emerald-700 hover:bg-emerald-500 focus:bg-emerald-500 text-white w-full">
+                    Enviar
+                </button>
+            </form>
+        </section>
+    </div>
 </template>
