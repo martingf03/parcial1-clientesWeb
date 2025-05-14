b3a7836c3bcd1e353e8050572d1260e5e8080787
Martin <martingf@gmail.com>
2025-05-14 13:48:13 -0300

    14/5 - 13:47, se agregó primera parte de lógica de auth

---
+++ src/components/auth/RegisterSection.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    name: "Register",
+    data() {
+        return {
+            user: {
+                email: "",
+                password: ""
+            }
+        }
+    },
+
+    methods: {
+        async handleSubmit() {
+
+        }
+    }
+
+}
+</script>
+
+<template>
+    <div class="w-2xl p-6 shadow rounded">
+        <form action="#" @submit.prevent="handleSubmit">
+            <div class="mb-4 flex flex-col">
+                <label for="email" class="mb-2">E-mail</label>
+                <input type="email" name="email" id="email" class="mb-2 border-gray-300 border-2 rounded h-8"
+                    v-model="user.email">
+            </div>
+
+            <div class="mb-4 flex flex-col">
+                <label for="password">Contraseña</label>
+                <input type="password" name="password" id="password" class="mb-2 border-gray-300 border-2 rounded h-8"
+                    v-model="user.password">
+            </div>
+            <button type="submit"
+                class="transition py-2 px-4 rounded bg-emerald-700 hover:bg-emerald-500 focus:bg-emerald-500 text-white w-full">
+                Crear cuenta
+            </button>
+        </form>
+    </div>
+
+</template>
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,11 +1,13 @@
 <script>
+import RegisterSection from '../components/auth/RegisterSection.vue';
 import MainH1 from '../components/MainH1.vue';
 export default {
     name: "Register",
-    components: {MainH1},
+    components: {MainH1, RegisterSection},
 }
 </script>
 
 <template>
     <MainH1>Registrarse</MainH1>
+    <RegisterSection />
 </template>
